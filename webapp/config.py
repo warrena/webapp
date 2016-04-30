4bd4904c3470f3f8fd47bdd5f999f5d817e4da5f
joshpitkofsky <pitkofsky.j@gmail.com>
2016-04-30 15:59:31 -0500

    added config

---
+++ webapp/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+    config.py
+    JP and Allie warren, 25 April 2016
+
+    Database login info for the "countries and violence"  web
+    app for CS 257, Spring 2016.
+'''
+
+# Change these values as appropriate for your postgresql setup.
+database = 'pitkofskyj'
+user = 'pitkofskyj'
+password = 'carpet333smile'
